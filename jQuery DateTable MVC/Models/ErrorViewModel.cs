1c31d1b929a67ecc9409464cbc4b4858a2d35245
Javier <javierdoriac@gmail.com>
2025-12-22 22:48:55 -0500

    Jquery Datatable MV

---
+++ jQuery DateTable MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace jQuery_DateTable_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
